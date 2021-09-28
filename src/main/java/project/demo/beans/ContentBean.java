b501454c9023a58c8b90324790e4f29c55b82030
JongGurlHan <gojkhan13@gmail.com>
2021-09-28 17:30:08 +0900

    0928 게시글작성 - 유효성 검사 및 오류메시지 설정

--- src/main/java/project/demo/beans/ContentBean.java
+++ src/main/java/project/demo/beans/ContentBean.java
@@ -3,15 +3,25 @@ package project.demo.beans;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class ContentBean {
 
     private int content_idx;
+
+    @NotBlank
     private String content_subject;
+
+    @NotBlank
     private String content_text;
+
     private String content_file;
+
     private int content_writer_idx;
+
     private int content_board_idx;
+
     private String content_date;
 }
--- src/main/java/project/demo/controller/BoardController.java
+++ src/main/java/project/demo/controller/BoardController.java
@@ -1,15 +1,22 @@
 package project.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import project.demo.beans.ContentBean;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	
 	@GetMapping("/main")
-	public String main() {
+	public String main(@RequestParam("board_info_idx") int board_info_idx,
+					   Model model) {
+		model.addAttribute("board_info_idx", board_info_idx);
+
 		return "board/main";
 	}
 
@@ -19,8 +26,17 @@ public class BoardController {
 	}
 
 	@GetMapping("/write")
-	public String write(){
+	public String write(@ModelAttribute("writeContentBean")ContentBean writeContentBean){
 		return "board/write";
+	}
+
+	@PostMapping("/write")
+	public String write(@Valid @ModelAttribute("writeContentBean")ContentBean writeContentBean, BindingResult result){
+		if(result.hasErrors()){
+			return "board/write";
+		}
+		return "board/write_success";
+
 	}
 
 	@GetMapping("/modify")
